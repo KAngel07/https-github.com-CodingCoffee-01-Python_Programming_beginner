da2f2db7740231b6fdb248988f28c6483ec556ab
KAngel07 <109204@mail.tmps.tp.edu.tw>
2025-04-08 19:51:30 +0800

    Create ex4-1.py

---
+++ ex4-1.py
@@ -0,0 +1,9 @@
+name = input("ANGEL")
+print(type (name))
+print("Hello, "+ name)
+
+age = int(input("11") )
+print(type(age))
+...
+height = float(input("Enter your height(m):"))
+print(type(height))
