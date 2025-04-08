7156d524e9419b0353085f11a2f3d9649b84ed95
KAngel07 <109204@mail.tmps.tp.edu.tw>
2025-04-08 20:58:31 +0800

    Create ex3-2.py

---
+++ ex3-2.py
@@ -0,0 +1,26 @@
+...
+text = "Hello python!"
+print(text)
+print(text[0])
+print(text[5])
+print(text[6])
+print(text[1])
+
+...
+a=10 > 5
+b=5> 8
+print(a and b , a or b, not b )
+
+...
+
+x, y = 7, 9
+print(x + y, x - y, x * y, x / y, x % y, x ** y, x // y)
+...
+
+fruits = ["apple", "banana", "cherry"]
+print("apple" in fruits, "mango" not in fruits)
+...
+
+x = 5
+x += 3  # x = x + 3
+print(x)
